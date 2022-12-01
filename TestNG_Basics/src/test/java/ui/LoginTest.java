f1f00ab08b569c8111fdd065ced8e6de3c7c48ee
Tsoko Malusi <tsokomalusi@outlook.com>
2022-12-01 22:44:10 +0200

    second commit

---
+++ TestNG_Basics/src/test/java/ui/LoginTest.java
@@ -0,0 +1,15 @@
+package ui;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	
+	@Test
+	public void loginTest()  {
+		
+	  System.out.println("Login is successful");	
+		
+	}
+
+}
