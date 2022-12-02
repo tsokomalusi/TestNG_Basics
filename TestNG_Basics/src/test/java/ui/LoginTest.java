8279874e6963578c2b13f84adf910d0e2f436a7d
Tsoko Malusi <tsokomalusi@outlook.com>
2022-12-02 16:57:47 +0200

    added a seccond test case and they are prioritized

--- TestNG_Basics/src/test/java/ui/LoginTest.java
+++ TestNG_Basics/src/test/java/ui/LoginTest.java
@@ -4,11 +4,20 @@ import org.testng.annotations.Test;
 
 public class LoginTest {
 	
+	//Prioritizing test cases using the priority attribute of @Test
+	//loginTest must execute first before logoutTest
 	
-	@Test
+	@Test(priority=1)
 	public void loginTest()  {
 		
 	  System.out.println("Login is successful");	
+		
+	}
+	
+	@Test(priority=2)
+	public void logoutTest()  {
+		
+	System.out.println("Logout is successful");	
 		
 	}
 
