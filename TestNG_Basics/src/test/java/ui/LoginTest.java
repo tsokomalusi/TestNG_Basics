98761db3165bf9e8d14e18f36557ef2df31c8d1c
Tsoko Malusi <tsokomalusi@outlook.com>
2022-12-02 17:12:40 +0200

    renamed the project name, added description atrribute to the test cases

--- TestNG_Basics/src/test/java/ui/LoginTest.java
+++ TestNG_Basics/src/test/java/ui/LoginTest.java
@@ -7,14 +7,14 @@ public class LoginTest {
 	//Prioritizing test cases using the priority attribute of @Test
 	//loginTest must execute first before logoutTest
 	
-	@Test(priority=1)
+	@Test(priority=1,description="This is a login test")
 	public void loginTest()  {
 		
 	  System.out.println("Login is successful");	
 		
 	}
 	
-	@Test(priority=2)
+	@Test(priority=2, description="This is a logout test")
 	public void logoutTest()  {
 		
 	System.out.println("Logout is successful");	
