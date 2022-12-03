036830feb432dadc32f25a09af8ea0dd751251f8
Tsoko Malusi <tsokomalusi@outlook.com>
2022-12-03 14:39:10 +0200

    added a new class and grouped test cases at method level

---
+++ TestNG_Basics/src/test/java/ui/GroupsDemoTest.java
@@ -0,0 +1,36 @@
+package ui;
+
+
+import org.testng.annotations.Test;
+
+public class GroupsDemoTest {
+	
+	//Grouping test cases at method level
+
+	@Test(priority=1, groups="regression")
+	public void test1()  {
+
+		System.out.println("Test1 Running");	
+	}
+
+	@Test(priority=2, groups="regression")
+	public void test2()  {
+
+		System.out.println("Test2 Running");	
+	}
+	
+	//a test case belonging to multiple groups
+	
+	@Test(priority=3, groups= {"regression", "bvt"})
+	public void test3()   {
+		
+		System.out.println("Test3 Running");
+	}
+	
+	@Test(priority=3, groups= "bvt")
+	public void test4()  {
+		
+		System.out.println("Test4 Running");
+	}
+
+}
