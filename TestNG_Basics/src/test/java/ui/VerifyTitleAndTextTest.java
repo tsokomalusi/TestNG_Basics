873c59671a67b2ed990a5b084af0d7c2c228d85f
Tsoko Malusi <tsokomalusi@outlook.com>
2022-12-03 11:22:16 +0200

    replaced HardAssert with SoftAssert in the test case

--- TestNG_Basics/src/test/java/ui/VerifyTitleAndTextTest.java
+++ TestNG_Basics/src/test/java/ui/VerifyTitleAndTextTest.java
@@ -3,28 +3,32 @@ package ui;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class VerifyTitleAndTextTest {
 	
-	//Introducing multiple HardAssert(assertions) into the test case(s)
-	//HardAssert stops script execution at that failed assertion pont, other remaining asserts
-	//and statments ahead won't be considered
+     //Introducing SoftAssert into the test case
+	 //SoftAssert ensures that script executes full without stopping
+	 //SoftAssert also reports mutliple assertion failure (assertAll() method is key)
+	//If both assertion points fail, then title message and text message in this test test case
+	//will be printed
 	
 	@Test
-	public void titleTest() throws InterruptedException {
+	public void titleAndTextTest() throws InterruptedException {
 		
+		SoftAssert softAssert =  new SoftAssert();
 		//String expectedTitle = "Electronics, Cars, Fashion, Collectibles & More | eBay";
 		
-		//make an error deliberately to see the effect of HardAssert
-		//THEN title message and/or textmessage get printed on the console
+		//create error to see the effect of SoftAssert
 		
-		String expectedTitle = "Electronics, rrrtCars, Fashion, Collectibles & More | eBay";
-		
-		String expectedText = "Search";
+        String expectedTitle = "Electronics, rrrtCars, Fashion, Collectibles & More | eBay";
+        
+       // String expectedText = "Search";
+        //make another assertion failure to see if SoftAssert will report it
+		String expectedText = "xxxxSearch";
 		String textMessage = "Text verification failed";
 		String titleMessage = "Title verification failed";
 		
@@ -32,15 +36,19 @@ public class VerifyTitleAndTextTest {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.ebay.com/");
 		String actualTitle = driver.getTitle();
-		
-		//if this verification point (assertion) fails executions stops immediately
-		//Other assertion points (hard assert) and statements too wont be considered
-		
-		Assert.assertEquals(actualTitle, expectedTitle, titleMessage);
+		System.out.println("Verifying title");
+		softAssert.assertEquals(actualTitle, expectedTitle, titleMessage);
 		String actualText = driver.findElement(By.xpath("//*[@id=\"gh-btn\"]")).getAttribute("value");
-		Assert.assertEquals(actualText, expectedText, textMessage);
-		
+		System.out.println("Verifying text");
+		softAssert.assertEquals(actualText, expectedText, textMessage);
+		System.out.println("Closing the browser");
 		driver.close();	
+		
+		//very key in reporting assertion failure(s),if omitted test script gets executed fully
+		// but assertion failure(s) are not reported
+		
+	    softAssert.assertAll();  
 	}
 
 }
+
