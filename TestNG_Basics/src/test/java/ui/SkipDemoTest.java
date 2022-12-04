b141c7288935c3b27953c9e7025f6c6cded62a24
Tsoko Malusi <tsokomalusi@outlook.com>
2022-12-04 10:22:33 +0200

    indented the code

--- TestNG_Basics/src/test/java/ui/SkipDemoTest.java
+++ TestNG_Basics/src/test/java/ui/SkipDemoTest.java
@@ -4,11 +4,11 @@ import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class SkipDemoTest {
-     
+
 	/**Using dummy test cases to show how test cases are skipped in testNG
 	//We have different approaches but each depends on the given scenario
-    **/
-	
+	 **/
+
 	//Boolean dataAvailable = true;
 	Boolean dataAvailable = false;
 
